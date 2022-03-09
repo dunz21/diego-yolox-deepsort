b43daba8746ecddf85c3f6dbcc661083b2b3da29
rohitkuk <rohit.kukreja01@gmail.com>
2022-03-09 15:33:53 +0530

    ..

--- dashboard.py
+++ dashboard.py
@@ -13,7 +13,10 @@ from flask import Flask, Response
 
 # Plotly-Dash Imports 
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
+import plotly.graph_objects as go
+import plotly.express as px
+
 from flask import Flask
 import dash_bootstrap_components  as dbc
 
@@ -29,7 +32,29 @@ app = Dash(__name__, server = server, external_stylesheets=[dbc.themes.BOOTSTRAP
 # Init Tracker
 tracker = Tracker(filter_classes= None, model = 'yolox-s', ckpt='weights/yolox_s.pth')
 
-Main = deque(maxlen = 1000)
+Main = []
+
+# Sunburst Data Function
+def build_hierarchical_dataframe(df, levels, value_column):
+    df_all_trees = pd.DataFrame(columns=['id', 'parent', 'value'])
+    for i, level in enumerate(levels):
+        df_tree = pd.DataFrame(columns=['id', 'parent', 'value'])
+        dfg = df.groupby(levels[i:]).sum()
+        dfg = dfg.reset_index()
+        df_tree['id'] = dfg[level].copy()
+        if i < len(levels) - 1:
+            df_tree['parent'] = dfg[levels[i+1]].copy()
+        else:
+            df_tree['parent'] = 'total'
+        df_tree['value'] = dfg[value_column]
+        df_all_trees = df_all_trees.append(df_tree, ignore_index=True)
+    total = pd.Series(dict(id='total', parent='',
+                              value=df[value_column].sum(),
+                              ))
+    df_all_trees = df_all_trees.append(total, ignore_index=True)
+    return df_all_trees
+
+
 
 # -------------------------------------------------Getting Video Feeds ------------------------------#
 
@@ -75,37 +100,200 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # ---------------------------------------------------------------------------------------------------#
+# Card Compnent
 
-videofeed = html.Img(src = "/video_feed")
+def create_card(Header, Value, cardcolor):
+    card = dbc.Col([
+        dbc.Card([
+            dbc.CardHeader(Header, style = {'text-align':'center'}),
+            dbc.Cardbody([
+                html.H3(Value, style = {'text-align':'center'})
+            ])
+        ], color = cardcolor, inverse=True, style = {
+            "width":"18rem",
+            'text-align':"center",
+            "vertical-align":"middle"
+        })
+    ])
+    return card
 
+
+# Video Feed Component
 videofeeds = dbc.Col(width=4, children =[
         html.Img(src = "/video_feed", style = {
             'max-width':'100%',
             'height':'auto',
             'display':'block',
             'margin-left':'auto',
-            'margin-right':'auto'
+            'margin-right':'auto'})]) 
 
-        })
-    ]
-
-) 
-
+# Header Component
 header = dbc.Col(width = 10,
     children = [ html.H1("Traffic Flow Management", style = {'text-align':'center'})]
 )
 
+# Grpahical Components
+figure1 = dbc.Col([dcc.Graph(id="live-graph1")], width=4)
+figure2 = dbc.Col(dcc.Graph(id="live-graph2"), width=4)
+piefig = dbc.Col(dcc.Graph(id="piefig"), width=4)
+dirfig = dbc.Col(dcc.Graph(id="dirfig"), width=4)
+sunfig = dbc.Col(dcc.Graph(id="sunfig"), width=4)
+speedfig = dbc.Col(dcc.Graph(id="sunfig"), width=8)
+infig = dbc.Col(dcc.Graph(id="infig"), width=4)
 
 
 
+fps = 0
+res = "A x B"
+stream = "Stream 1"
+average_speed = 0
+previous_av_speed = 0
+
+
+"""
+This Function Takes the input as n_interval and will execute by itself after a certain time
+It outputs the figures 
+
+"""
+@app.callback([
+    Output('live-graph1', 'figure'),
+    Output('live-graph2', 'figure'),
+    Output('cards', 'children'),
+    Output('piefig', 'figure'),
+    Output('dirfig', 'figure'),
+    Output('sunfig', 'figure'),
+    Output('speedfig', 'figure'),
+    Output('infig', 'figure'),
+
+    ],
+        [
+            Input('visual-update', 'n_intervals')
+        ]   
+)
+def update_visuals(n):
+    global average_speed, previous_av_speed
+    fig1     = go.FigureWidget()
+    fig2     = go.FigureWidget()
+    piefig   = go.FigureWidget()
+    dirfig   = go.FigureWidget()
+    sunfig   = go.FigureWidget()
+    speedfig = go.FigureWidget()
+    infig    = go.FigureWidget()
+
+
+    # Dataset Creation a
+    vehicleslastminute = 0
+    vehiclestotal = 0
+    if len(df) !=0:        
+        df = pd.DataFrame(Main)
+        df1 = df.copy()
+        df['count'] = 1
+        average_speed = int(df["Speed"].mean())
+        # Database Transformations
+        df = df.pivot_table(index = ['Time'], columns = 'Category', aggfunc = {'Category':"count"}).fillna(0)
+        df.columns = df.columns.droplevel(0)
+        df = df.reset_index()
+        df.Time = pd.to_datetime(df.Time)
+        columns = list(df.columns)
+        columns.remove('Time')
+
+        # Direction Datset
+        dirdf = df.groupby(['direction']).agg({"Speed": np.mean}).reset_index()
+
+        # Sunburst Dataset
+        df_all_trees = build_hierarchical_dataframe(df=df1, levels = ["Category",'direction'], value_column = "count")
+     
+        # Speed Dataset
+        df1 = df1.pivot_table(index = ['Time'], columns = 'Category', aggfunc = {'Speed':np.mean}).fillna(0)
+        df1.columns = df1.columns.droplevel(0)
+        df1 = df1.reset_index()
+        df1.Time = pd.to_datetime(df1.Time)
+        columns1 = list(df1.columns)
+        columns1.remove('Time')
+
+        # Speed Fig Add Scatter 
+        for col in columns1:
+            speedfig.add_scatter(name = col, x = df1['Time'], y = df1[col], fill = "tonexty", line_shape = "spline")
+
+        
+        # Looping for adding scatter for each category
+        values_sum = []
+        for col in columns:    
+            fig1.add_scatter(name = col,x=df['Time'], y=df[col], fill='tonexty', showlegend=True, line_shape='spline')
+            fig2.add_scatter(name = col,x=df['Time'], y=df[col].cumsum(), fill='tonexty', showlegend=True, line_shape='spline')
+            vehicleslastminute += df[col].values[-1]
+            vehiclestotal += df[col].cumsum().values[-1]
+            values_sum.append(df[col].sum())
+        
+        piefig = px.pie(
+            labels = columns, names = columns, values = values_sum, hole = 0.5,
+            title = "Traffic Distribution - Vehicle Type",
+            color_discrete_sequence= px.colors.sequential.Agsunset, opacity=0.85 
+        )
+
+        dirfig = px.bar(dirdf, y = "direction", x = "Speed", color = "direction", orientation="h",hover_name="direction",
+                color_discrete_map={
+                "North" : "rgba(188,75,128,0.8)",
+                "South" : 'rgba(26,150,65,0.5)',
+                "East"  : 'rgba(64,167,216,0.8)',
+                "West"  : "rgba(218,165,32,0.8)"},
+            title = "Average Speed Direction Flow"
+
+        )
+
+    sunfig = go.Sunburst(
+        labels = df_all_trees['id'],
+        parents = df_all_trees['parent'],
+        values = df_all_trees['values'],
+        branchvalues = 'total',
+        textinfo = 'label+percent entry',
+        opacity = 0.85
+    )
+
+    cards = [
+        create_card(Header = "Vehicles This Minute", Value = vehicleslastminute, cardcolor = "primary"),
+        create_card(Header = "Total Vehicles", Value = vehiclestotal, cardcolor = "info"),
+
+        create_card(Header = "Frames Per Second", Value = fps, cardcolor = "secondary"),
+        create_card(Header = "Resolution", Value = res, cardcolor = "warning"),
+        create_card(Header = "Stream", Value = stream, cardcolor = "danger"),
+
+
+    ]
+    
+    infig = go.FigureWidget(
+        go.Indicator(
+            domain = {'x':[0,1], 'y':[0,1]},
+            value = average_speed,
+            mode = "gauge+number+delta",
+            title = {'text':"Average Speed Km/h"},
+            delta = {'reference': previous_av_speed},
+            gauge = {'axis': {'range': [None, 50]},
+             'bar': {'color': "rgba(50,251,226,60)"},
+
+                'steps' : [
+                    {'range': [0, 15], 'color': 'rgba(0,0,0,0)'},
+                    {'range': [15, 50], 'color':'rgba(0,0,0,0)'}],
+                'threshold' : {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 45}}
+
+        )
+    )
+
+     
+
+    return fig1, fig2 , cards, piefig, dirfig, sunfig, speedfig, infig
 
 
 
 app.layout = html.Div([
-    dbc.Row([header]), #Header
-    dbc.Row([]), #Cards
-    dbc.Row([videofeeds]), #VideoFeed and 2 Graphs
+    # Input for all the updating visuals
+    dcc.Interval(id='visual-update',interval=2000,n_intervals = 0),
 
+    dbc.Row([header]), #Header
+    dbc.Row(id="cards"), #Cards
+    dbc.Row([videofeeds, figure1, figure2]), #VideoFeed and 2 Graphs
+    dbc.Row([piefig, sunfig ,dirfig]), #Header
+    dbc.Row([speedfig, infig]), #Header
 
 ])
 
