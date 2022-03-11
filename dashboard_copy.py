bdb153470a04a593ec9d3a3acf6ef1a3243787f7
rohitkuk <rohit.kukreja01@gmail.com>
2022-03-11 22:29:10 +0530

    ..

--- dashboard_copy.py
+++ dashboard_copy.py
@@ -158,7 +158,8 @@ header = dbc.Col( width = 10, children=[
                     'text-align': 'center;',
                     'background': '#1abc9c;',
                     'color': 'white;',
-                            },children = [html.H1("Traffic Flow Management System", style = { 'font-size': '4.5rem','text-align': 'center'})
+                    'font-size': '4.5rem','textAlign': 'center'
+                            },children = [html.H1("Traffic Flow Management System", style = { 'font-size': '4.5rem','textAlign': 'center'})
                             
                         ])
                     ])
