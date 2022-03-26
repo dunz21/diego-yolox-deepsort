0b0ae8e483d9d98685aef2b2cc5babe81d646dc3
rohitkuk <rohit.kukreja01@gmail.com>
2022-03-26 20:27:59 +0530

    ..

--- dashboard.py
+++ dashboard.py
@@ -239,6 +239,17 @@ dbc.Offcanvas(
 
 ])
 
+@app.callback(
+    Output('Offcanvas-scrollable', "is_open"),
+    Input('open-offcanvas-scrollable', "n_clicks"),
+    State("offcanvas-scrollable", "is_open")
+)
+
+def toggle_offcavas_scrollable(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
+
 
 """
 This Function Takes the input as n_interval and will execute by itself after a certain time
